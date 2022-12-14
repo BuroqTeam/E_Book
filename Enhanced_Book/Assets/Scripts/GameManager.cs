31ef26a9b57d22f0adf958ec0fc23fbfc691919d
Kayumiy <qayum.mus@mail.ru>
2022-12-14 11:43:25 +0500

    Media buttons

--- Enhanced_Book/Assets/Scripts/GameManager.cs
+++ Enhanced_Book/Assets/Scripts/GameManager.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public List<string> GameScenes;
+    public List<string> QuizScenes;
+    public List<string> MediaScenes;
+
+    public static GameManager Instance;
+
+
+    private void Awake()
+    {
+        Instance = this;
+
+
+    }
+
+
     void Update()
     {
         if (Input.GetKey("escape"))
