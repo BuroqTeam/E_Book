37829b7bc9681eb21738a076cbd14752babe3d6d
RustamMuhammadFathulloh <blackgoold2001@gmail.com>
2022-12-14 19:19:36 +0500

    FinishPanel bitdi.
    Game1 uchun tayyorlangan Finish Panel bitdi.

--- Enhanced_Book/Assets/Team/Fathulloh/ScriptsForFathulloh/FinishPanel/GameResultPanel.cs
+++ Enhanced_Book/Assets/Team/Fathulloh/ScriptsForFathulloh/FinishPanel/GameResultPanel.cs
@@ -1,32 +1,57 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using YuzlikFathulloh;
 
 public class GameResultPanel : MonoBehaviour
 {
     public GameObject BadgeObj;
+    public GameObject BadgeText;
+    public GameObject TimerObject;
+    public TMP_Text TimeText;
 
-    public Dictionary<string, Sprite> ResultDict = new();
 
     public Sprite GoldSprite, SilverSprite, BronzeSprite, LoseSprite;
-    public string GoldStr = "Barakalla", SilverStr = "Zo'r", BronzeStr = "Yaxshi", LoseStr = "Yomon";
+    private const string GoldStr = "Barakalla !";
+    private const string SilverStr = "Zo'r !";
+    private const string BronzeStr = "Yaxshi !";
+    private const string LoseStr = "Yomon !";
 
 
     void Start()
     {
-        MakeResultDict();
+        
     }
 
 
-    public void MakeResultDict()
+    
+    public void ChooseBadge()
     {
-        ResultDict.Add(GoldStr, GoldSprite);
-        ResultDict.Add(SilverStr, SilverSprite);
-        ResultDict.Add(BronzeStr, BronzeSprite);
-        ResultDict.Add(LoseStr, LoseSprite);
+        float spendingTime = TimerObject.GetComponent<Timer>().currentTime;
 
+        if (spendingTime > 200)        {
+            BadgeObj.GetComponent<Image>().sprite = GoldSprite;
+            BadgeText.GetComponent<TMP_Text>().text = GoldStr;
+        }
+        else if (spendingTime > 120)
+        {
+            BadgeObj.GetComponent<Image>().sprite = SilverSprite;
+            BadgeText.GetComponent<TMP_Text>().text = SilverStr;
+        }
+        else if (spendingTime > 0)
+        {
+            BadgeObj.GetComponent<Image>().sprite = BronzeSprite;
+            BadgeText.GetComponent<TMP_Text>().text = BronzeStr;
+        }
+        else if (spendingTime <= 0)
+        {
+            BadgeObj.GetComponent<Image>().sprite = LoseSprite;
+            BadgeText.GetComponent<TMP_Text>().text = LoseStr;
+        }
+
+        TimeText.text = TimerObject.transform.GetChild(0).GetComponent<TMP_Text>().text;
     }
-
 
     
 }
