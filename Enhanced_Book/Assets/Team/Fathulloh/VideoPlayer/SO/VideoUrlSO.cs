2c56ad69f68bbff5920b886607e05a6cbfeea050
RustamMuhammadFathulloh <blackgoold2001@gmail.com>
2023-01-29 14:14:23 +0500

    New Asset LightShaft
    Video Player Style Chosen for video scene.
    Scriptable Object is created.
    Sprites added for No Internet notification.

---
+++ Enhanced_Book/Assets/Team/Fathulloh/VideoPlayer/SO/VideoUrlSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NextSceneData", menuName = "ScriptableObjects/VideoEvent")]
+public class VideoUrlSO : ScriptableObject
+{
+    public string sceneName;
+    //public int intNum;
+
+
+}
