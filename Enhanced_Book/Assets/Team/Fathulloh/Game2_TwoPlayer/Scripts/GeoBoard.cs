055a3777a8df444a848bcb38c9dff0115cf769ae
RustamMuhammadFathulloh <blackgoold2001@gmail.com>
2023-05-31 08:56:25 +0500

    Game2
    Game2 ni multiplayer qilish zarur.
    Buning uchun bir qancha scriptlar qaytadan yozilayabdi.

---
+++ Enhanced_Book/Assets/Team/Fathulloh/Game2_TwoPlayer/Scripts/GeoBoard.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game2_TwoPlayer
+{
+    public class GeoBoard : MonoBehaviour
+    {
+        public GameObject ParentForDots;
+        public GameObject HookPrefab;
+
+        public int GridX, GridY;
+        public float Xdistance, Ydistance;
+
+        public List<GameObject> Hooks;
+        public List<GameObject> Lines;
+
+
+        void Start()
+        {
+            MakeBoard();
+        }
+
+
+        void MakeBoard()
+        {
+            float xInitial = 0.1f + gameObject.transform.position.x - (float)GridX / 2 * Xdistance + (float)Xdistance / 2;
+            float yInitial = 0.1f + gameObject.transform.position.y - (float)GridY / 2 * Ydistance + (float)Ydistance / 2;
+
+            for (int i = 0; i < GridX; i++)
+            {
+                float xPos = xInitial + i * Xdistance;
+                for (int j = 0; j < GridY; j++)
+                {
+                    float yPos = yInitial + j * Ydistance;
+                    GameObject obj = Instantiate(HookPrefab, ParentForDots.transform);
+                    Hooks.Add(obj);
+                    obj.transform.position = new Vector3(xPos, yPos);                    
+                }
+            }
+
+        }
+
+
+    }
+}
